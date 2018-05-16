049d908b24c9a87245575baa2863feb3bbbf3e6d
youmustmurder <slava.ustinow@gmail.com>
2018-05-16 17:28:25 +0500

    Initial commit

---
+++ vuetube/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from './components/VideoPlayer'
+export default {
+  name: 'App',
+  components: {
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
