bd4dad54c5836c233e5286032542e422b70db808
youmustmurder <slava.ustinow@gmail.com>
2018-05-16 18:46:16 +0500

    vuex, decomposiotion

---
+++ vuetube/src/components/VideoContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="video-container">
+    <iframe :src="activeVideo.youtubeURL" width="640" height="360" frameborder="0" allow="autoplay;encrypted-media" allowfullscreen></iframe>
+    <h3>{{ activeVideo.title }}</h3>
+    <div class="row">
+      <p>{{ activeVideo.views }} views</p>
+      <p>{{ activeVideo.likes }} <button @click="addLike()">Like</button></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    activeVideo () {
+      return this.$store.state.activeVideo;
+    }
+  },
+  methods: {
+    addLike() {
+      this.$store.commit('increment_like');
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
